e72296c3d5fef35c09756e0ed2c9deafa5b98093
Shikhar Mani Tripathi <shiktr1785@users.noreply.github.com>
2024-12-25 15:00:38 +0530

    new file

---
+++ booth_mul.sv
@@ -0,0 +1,11 @@
+module booth (
+    input logic clk,
+    input logic rst,
+    input logic [31:0] multiplicand,
+    input logic [31:0] multiplier,
+    output logic [63:0] product,
+    output logic ready,
+);
+
+    
+endmodule
